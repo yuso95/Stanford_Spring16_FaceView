7647d8199c78eb5ab2ebb2a260f24340e908002d
Ousmane Y. Abdou <Yuso@NS-YUSO-MacBook-Pro.local>
2016-12-29 12:42:17 -0500

    init the FaceVC and the view FaceView

---
+++ FaceView/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  FaceView
+//
+//  Created by Younoussa Ousmane Abdou on 12/29/16.
+//  Copyright © 2016 Younoussa Ousmane Abdou. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ FaceView/FaceViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  FaceView
+//
+//  Created by Younoussa Ousmane Abdou on 12/29/16.
+//  Copyright © 2016 Younoussa Ousmane Abdou. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+
+}
+
